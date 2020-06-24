ddb27713291cfad5221b0f191fb60ccff8b43663
Adamski B <aboulton@live.co.uk>
2020-06-24 21:27:24 +0100

    handling unordered lists in react

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake({name}){
-  return(
-  <h1>{name}</h1>
-  )
-}
+const lakesList = ["lake one", "lake two", "lake three"];
 
-function App(){
-  return(
-    <div>
-      <Lake name="Lake Tahoe"/>
-      <Lake name="Lake Idaho" />
-      <Lake name="Lake Windamere"/>
-    </div>
-  )
+function App( { lakes } ){
+  return (
+     <ul>
+       {lakes.map(lake => (
+         <li>{lake}</li>
+       ))}
+     </ul>
+    
+  );
 }
 
 ReactDOM.render(
-  <App />,
+  <App lakes={lakesList}/>,
   document.getElementById('root')
 );
 
