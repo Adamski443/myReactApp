519a6ce43cd06bb62597ec67cdaf84252f58c19e
Adamski B <aboulton@live.co.uk>
2020-06-24 20:27:36 +0100

    Playing with properties (props)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Hello({library, number}){
+  return (
+  <div>
+  <h1>Welcome to React!!!</h1>
+  <p>This is coolio!!!</p>
+  <p>{library} is the library</p>
+  <p>The number is {number}</p>
+  </div>
+  );
+}
+
 ReactDOM.render(
-    React.createElement("h1", {style: {color: "red"}}, "Just a test site"),
+  <Hello library="React" number={3}/>,
   document.getElementById('root')
 );
 
